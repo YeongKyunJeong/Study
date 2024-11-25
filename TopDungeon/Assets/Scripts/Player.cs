3a5ecfe3ca92faf8753dce72e8cfd2604204f8b1
ykjeong@creverse.com <ykjeong@creverse.com>
2024-11-25 16:33:13 +0900

    [Top Dungeon] 1.0.1 스프라이트 Atlas 저장

---
+++ TopDungeon/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(BoxCollider2D))]
+public class Player : MonoBehaviour
+{
+    private BoxCollider2D boxCollider;
+    private Vector3 moveDelta;
+
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnCollectCoin()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        // Reset MoveDelta
+        moveDelta = new Vector3(x, y, 0);
+
+        // Swap sprite direction, wether you're going right or left
+        if (moveDelta.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if (moveDelta.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+
+        // Make this thing move;
+        transform.Translate(moveDelta * Time.deltaTime);
+    }
+}
+
