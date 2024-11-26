39ffcf836f49b5e68495cc10612a425cb3a5e26b
ykjeong@creverse.com <ykjeong@creverse.com>
2024-11-26 18:10:32 +0900

    [Top Dungeon] 1.0.2 캐릭터 이동, 카메라 이동 구현

--- TopDungeon/Assets/Scripts/Player.cs
+++ TopDungeon/Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
 
     private void Start()
     {
@@ -36,8 +37,22 @@ public class Player : MonoBehaviour
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        // Make this thing move;
-        transform.Translate(moveDelta * Time.deltaTime);
+        // Make sure we can move in this direction, by casting a box first. If the box returns null, we're free to move
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Make this thing move;
+            transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+
+        }
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x, 0), Mathf.Abs(moveDelta.x * Time.deltaTime)/*±ª¿Ã?*/, LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Make this thing move;
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+
+        }
+
     }
 }
 
