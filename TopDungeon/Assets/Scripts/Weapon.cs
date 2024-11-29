99f44761a7be5ccaddb193891f80f327ef7d728a
YeongKyunJeong <jjbykk9999@snu.ac.kr>
2024-11-30 01:21:37 +0900

    [Top Dungeon] 1.0.35 공격 애니메이션 구현

--- TopDungeon/Assets/Scripts/Weapon.cs
+++ TopDungeon/Assets/Scripts/Weapon.cs
@@ -13,6 +13,7 @@ public class Weapon : Collidable
     private SpriteRenderer spriteRenderer;
 
     // Swing
+    private Animator anim;
     private float cooldown = 0.5f;
     private float lastSwing;
     // Coroutine È°¿ë
@@ -23,6 +24,7 @@ public class Weapon : Collidable
     {
         base.Start();
         spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     protected override void Update()
@@ -67,7 +69,7 @@ public class Weapon : Collidable
 
     private void Swing()
     {
-        Debug.Log("Swing");
+        anim.SetTrigger("Swing");
     }
 
     IEnumerator SwingCoroutine()
