77b321baefc8f537850e73d4f4c311b74c220b4d
npckero <npckero@gmail.com>
2024-12-31 22:12:50 +0900

    241231 수업내용

--- BrickBreaker/Assets/Scripts/Paddle.cs
+++ BrickBreaker/Assets/Scripts/Paddle.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,17 +10,23 @@ public class Paddle : MonoBehaviour
     private PaddleHandler paddleHandler;
 
     public float speed = 15f;
-    public Rigidbody2D rigidBody { get; private set; }
+    // public Rigidbody2D rigidBody { get; private set; }
 
     public Vector2 firstPosition { get; private set; }
 
+    public Vector3 velocity;
+
+    public float dampingTime = 0.1f;
+
+    public float stackTime = 0f;
+    
     public void Initialize()
     {
-        if (rigidBody == null)
-        {
-            rigidBody = GetComponent<Rigidbody2D>();
-
-        }
+        // if (rigidBody == null)
+        // {
+        //     rigidBody = GetComponent<Rigidbody2D>();
+        //
+        // }
         firstPosition = transform.position;
 
         if (paddleHandler == null)
@@ -37,18 +44,39 @@ public class Paddle : MonoBehaviour
         if (moveDirection == Vector2.zero)
         {
             //rigidBody.velocity = Vector2.zero;
+            // stackTime = 0f;
             return;
         }
         else
         {
-            rigidBody.velocity = moveDirection * speed;
+            // rigidBody.velocity = moveDirection * speed;
+            velocity = moveDirection;
         }
+
+        
+    }
+
+    private void Update()
+    {
+        transform.position += velocity * (speed * Time.deltaTime);
+        
+        // todo: ÁÂ¿ì ÀÌÅ» Á¦ÇÑ
+        
+        
+        velocity = Vector3.SmoothDamp(velocity, Vector2.zero, ref velocity, dampingTime);
+        
+        // if(stackTime < dampingTime)
+        // {
+        //     stackTime += Time.deltaTime;
+        //     velocity = Vector3.Lerp(velocity, Vector2.zero, stackTime / dampingTime);
+        // }
+        
     }
 
     public void ResetPaddle()
     {
         transform.position = firstPosition;
-        rigidBody.velocity = Vector2.zero;
+        //rigidBody.velocity = Vector2.zero;
     }
 
 
