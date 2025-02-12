c1f7b7432bc749b8d3ef564bb7ab5fe3ac3d56c2
YeongKyunJeong <jjbykk9999@snu.ac.kr>
2025-02-13 01:30:50 +0900

    [TDP] feat : Add turret enemy targeting and rotating logic

--- TDP/Assets/Scripts/Creature/Turret.cs
+++ TDP/Assets/Scripts/Creature/Turret.cs
@@ -11,6 +11,19 @@ namespace TDP
         public Transform target;
         private Collider[] detectedColldiers;
         private Coroutine detectingCoroutine;
+        private float distanceToEnemy;
+        private float closestDistance;
+        private int closestDistIndex;
+        private Vector3 dir;
+        private Vector3 rotation;
+        private Quaternion targetQuaternion;
+
+        #region Turret Object Motion Control
+
+        [SerializeField] private Transform rotatingPart;
+
+        #endregion
+
 
         #region Turret Data
         public float range;
@@ -19,13 +32,38 @@ namespace TDP
 
         #endregion
 
+        public float turnSpeed;
         private float detectionInterval;
         private LayerMask enemyLayerMask; // bitMask
         private WaitForSeconds detectionWaitForSec;
         private WaitForSeconds attackWaitForSec;
 
+        // temp
+        private void Start()
+        {
+            FirstPoolingInitialize();
+            SetTurretData();
+        }
+
+        private void Update()
+        {
+            if (target == null)
+            {
+                return;
+            }
+            else
+            {
+                dir = target.position - transform.position;
+                //rotation = Quaternion.LookRotation(dir).eulerAngles;
+                targetQuaternion = Quaternion.LookRotation(dir);
+                rotation = Quaternion.Lerp(rotatingPart.rotation, targetQuaternion, Time.deltaTime * turnSpeed).eulerAngles;
+                rotatingPart.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+            }
+        }
+
         public void FirstPoolingInitialize()
         {
+            turnSpeed = 10f;
             detectionInterval = 0.125f;
             enemyLayerMask = LayerMask.GetMask("Enemy");
             detectionWaitForSec = new WaitForSeconds(detectionInterval);
@@ -54,15 +92,34 @@ namespace TDP
 
         private void UpdateTarget()
         {
+            detectedColldiers = Physics.OverlapSphere(transform.position, range, enemyLayerMask);
+            closestDistance = range;
 
+            if (detectedColldiers.Length > 0)
+            {
+                for (int i = 0; i < detectedColldiers.Length; i++)
+                {
+                    distanceToEnemy = Vector3.Distance(detectedColldiers[i].transform.position, transform.position); // To do : Change distance from turret to distance to goal
+                    if (distanceToEnemy < closestDistance)
+                    {
+                        closestDistance = distanceToEnemy;
+                        closestDistIndex = i;
+                    }
+                }
+                target = detectedColldiers[closestDistIndex].transform;
+            }
+            else
+            {
+                target = null;
+            }
+            return;
         }
 
         private IEnumerator DetectEnemyCoroutine()
         {
             while (true)
             {
-                detectedColldiers = Physics.OverlapSphere(transform.position, range, enemyLayerMask);
-                // To do : Add target chosing logic
+                UpdateTarget();
 
                 if (target == null)
                 {
