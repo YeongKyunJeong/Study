40d55de55f7f4e2d9f24a55aba11fcc1f9ac938f
DESKTOP-I0CEP2K\user <stararia9999@gmail.com>
2025-05-26 18:09:14 +0900

    [RSP2] feat : Work on adding bullet and range attack functionality

---
+++ RSP2/Assets/ExternalAssets/Scripts/Characters/Player/StateMachines/States/ActionState/Attack/RangeAttackingStateForPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RSP2
+{
+    public class RangeAttackingStateForPlayer : BaseAttackStateForPlayer
+    {
+
+
+        public RangeAttackingStateForPlayer(Player _player, ActionStateMachineForPlayer _stateMachine) : base(_player, _stateMachine)
+        {
+        }
+
+
+    }
+}
