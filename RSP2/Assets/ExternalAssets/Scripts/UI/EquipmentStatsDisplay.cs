e6b508ea446a0bf38a6650b7d86c48ae8cc96891
YeongKyunJeong <jjbykk9999@snu.ac.kr>
2025-07-01 00:19:52 +0900

    [RSP2] feat : Work on adding stats display on equipment window

---
+++ RSP2/Assets/ExternalAssets/Scripts/UI/EquipmentStatsDisplay.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace RSP2
+{
+    public class EquipmentStatsDisplay : MonoBehaviour
+    {
+        [field: SerializeField] private TextMeshProUGUI LevelTMP;
+        [field: SerializeField] private TextMeshProUGUI ExpTMP;
+        [field: SerializeField] private TextMeshProUGUI HPTMP;
+        [field: SerializeField] private TextMeshProUGUI MPTMP;
+        [field: SerializeField] private TextMeshProUGUI StaminaTMP;
+        [field: SerializeField] private TextMeshProUGUI BaseAttackTMP;
+        [field: SerializeField] private TextMeshProUGUI WeaponAttackTMP;
+        [field: SerializeField] private TextMeshProUGUI AttackSpeedTMP;
+        [field: SerializeField] private TextMeshProUGUI BaseDefenseTMP;
+        [field: SerializeField] private TextMeshProUGUI ArmorDefenseTMP;
+        [field: SerializeField] private TextMeshProUGUI MovementSpeedTMP;
+
+    }
+}
