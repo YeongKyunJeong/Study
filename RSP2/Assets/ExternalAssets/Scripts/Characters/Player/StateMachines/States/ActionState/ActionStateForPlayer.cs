77f7f840e0eaaa2d425ba1f449fb5e66eaf8c0b3
DESKTOP-I0CEP2K\user <stararia9999@gmail.com>
2025-07-14 17:26:55 +0900

    [RSP2] fix : Fix the problem that player doesn't transit to falling state after dash to air

--- RSP2/Assets/ExternalAssets/Scripts/Characters/Player/StateMachines/States/ActionState/ActionStateForPlayer.cs
+++ RSP2/Assets/ExternalAssets/Scripts/Characters/Player/StateMachines/States/ActionState/ActionStateForPlayer.cs
@@ -215,7 +215,7 @@ namespace RSP2
         {
             if (slopeNormalVector.y < -0.98f)
             {
-                fallingThreshold = 7 * Physics.gravity.y * Time.deltaTime;
+                fallingThreshold = 3 * Physics.gravity.y * Time.deltaTime;
                 if (!controller.isGrounded && (fallingVelocityVector.y < fallingThreshold))
                 {
                     return true;
