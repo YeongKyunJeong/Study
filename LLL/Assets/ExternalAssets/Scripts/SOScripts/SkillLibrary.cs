98ae38148f1a18c394bc9935d8d8f0cd57d8ce84
YeongKyunJeong <jjbykk9999@snu.ac.kr>
2026-03-31 23:16:19 +0900

    [LLL] feat : Work on adding skill data

---
+++ LLL/Assets/ExternalAssets/Scripts/SOScripts/SkillLibrary.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace LLL
+{
+    [CreateAssetMenu(fileName = "SkillLibrary", menuName = "SO/Library/SkillLibrary")]
+    public class SkillLibrary : ScriptableObject
+    {
+        [field: SerializeField] public SkillData[] SkillData;
+    
+    }
+}
